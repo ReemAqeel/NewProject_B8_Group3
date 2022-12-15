5a2e7f12d7051f2513daed041f0f578b868b5dce
ReemAqeel <ReemAQ23@outlook.com>
2022-12-15 22:47:55 +0300

    printing message

--- src/lab1/Lab1.java
+++ src/lab1/Lab1.java
@@ -16,6 +16,7 @@ public class Lab1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello my name is Reem ");
     }
     
 }
