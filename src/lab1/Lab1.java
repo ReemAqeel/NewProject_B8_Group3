bd1338fd6e3144208c0a75b6e10dc4a088bc4bb8
AtheerAlzahrani <120296653+AtheerAlzahrani@users.noreply.github.com>
2022-12-16 10:14:17 +0300

    Update Lab1.java
    Simple print statement

--- src/lab1/Lab1.java
+++ src/lab1/Lab1.java
@@ -17,6 +17,7 @@ public class Lab1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello my name is Reem ");
+        System.out.println(" Hi my name is Atheer Alzahrani ");
     }
     
 }
