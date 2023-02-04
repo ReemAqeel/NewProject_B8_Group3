d73d26b864a147db2d8693593ed8e270cd126f6f
ReemAqeel <ReemAQ23@outlook.com>
2023-02-04 11:40:03 +0300

    Main method updated to menu

--- SchoolSystem/src/schoolsystem/SchoolSystem.java
+++ SchoolSystem/src/schoolsystem/SchoolSystem.java
@@ -1,31 +1,55 @@
-
 package schoolsystem;
+
 import java.util.*;
 
 public class SchoolSystem {
 
-//Add courses to students method 
-    public static void ADDCOURSE(ArrayList<String> STDschedual,String courseName){
-        if(STDschedual.contains(courseName)){
-            System.out.println("The course is already in the student schedual");
-        }else{
-            STDschedual.add(courseName);
-            System.out.println("Course is added to student schedual ");
-        }
-    }
-    /////////////////////////////////////////
-    
     public static void main(String[] args) {
-        // TODO code application logic here
-        ArrayList STDschedual=new ArrayList<String>();
-  
-        //taking course name from the user (teacher,adminstrator,student)
-        Scanner user=new Scanner(System.in);
-        System.out.print("please enter name of the course you wish to add in your schedual: ");
-        String courseName=user.next();
-        //sending to the method add course 
-        ADDCOURSE(STDschedual,courseName);
-        
+        //variables to send to methods 
+        ArrayList STDschedual = new ArrayList<String>();
+        String courseName;
+        //taking id and pass from user 
+        Scanner user = new Scanner(System.in);
+        System.out.print("Please enter your ID: ");
+        int id = user.nextInt();
+        System.out.print("Please enter your password: ");
+        int password = user.nextInt();
+        System.out.println("----------------------------------------------");
+        System.out.println("#Hello & Welcome to the High school Education System#");
+        System.out.println("----------------------------------------------\n");
+
+        //manue 
+        for (int i = 1; i > 0; i++) {
+            System.out.println("Please select from the following: ");
+            System.out.println("----------------------------------------------");
+            System.out.println("1. ADD COURSE TO STUDENT SCHEDUAL ");
+            System.out.println("2. DELETE COURSE TO STUDENT SCHEDUAL ");
+            System.out.println("3. VEIW COURSES TO REGISTOR");
+            System.out.println("4. ");
+            System.out.println("5. EXIT");
+            System.out.println("----------------------------------------------");
+
+            int selection = user.nextInt();
+
+            //sending to add course method in school function class
+            if (selection == 1) {
+                System.out.print("Please Enter the name of the course you wish to add: ");
+                courseName = user.next();
+                SchoolFunctions.ADDCOURSE(STDschedual, courseName);
+
+                //sending to add course method in school function class
+            } else if (selection == 2) {
+
+            } else if (selection == 3) {
+
+            } else if (selection == 4) {
+
+            } else if (selection == 5) {
+                System.out.println("Thank you for using our system, have a nice day :)");
+                break;
+            } else {
+                System.out.println("Please try again");
+        }
+      }
     }
-    
 }
