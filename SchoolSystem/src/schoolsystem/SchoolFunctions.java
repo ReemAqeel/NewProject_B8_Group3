38b4d4ec4645f59eaf8cf0c6471922648b1c2856
ReemAqeel <ReemAQ23@outlook.com>
2023-02-05 16:34:46 +0300

    view courses method done by nada

--- SchoolSystem/src/schoolsystem/SchoolFunctions.java
+++ SchoolSystem/src/schoolsystem/SchoolFunctions.java
@@ -47,5 +47,66 @@ public class SchoolFunctions {
         }
         
     }
-    
+      ////////////////////////////////////////////////
+     
+     //view courses to registor 
+       public static void VIEWCOURSES(int grade){
+        //array of courses that grade 10 takes 
+        ArrayList grade10=new ArrayList<String>();
+        grade10.add("MATH-110");
+        grade10.add("CHEMISTRY-210");
+        grade10.add("PHYSICS-310");
+        grade10.add("BIOLOGY-410");
+        grade10.add("HISTORY-510");
+        grade10.add("ENGLISH-610");
+        grade10.add("SOCIAL_STUDIES-710");
+        grade10.add("ISLAMIC-810");
+        
+        //array of courses that grade 11 takes
+        ArrayList grade11=new ArrayList<String>();
+        grade11.add("MATH-111");
+        grade11.add("CHEMISTRY-211");
+        grade11.add("PHYSICS-311");
+        grade11.add("BIOLOGY-411");
+        grade11.add("HISTORY-511");
+        grade11.add("ENGLISH-611");
+        grade11.add("BUSNIESS-711");
+        grade11.add("ISLAMIC-811");
+        
+        // array of courses that grade 12 takes 
+         ArrayList grade12=new ArrayList<String>();
+        grade12.add("CALCULAS-112");
+        grade12.add("CHEMISTRY-212");
+        grade12.add("PHYSICS-321");
+        grade12.add("BIOLOGY-412");
+        grade12.add("HISTORY-512");
+        grade12.add("ENGLISH-612");
+        grade12.add("BUSNIESS-712");
+        grade12.add("ISLAMIC-812");
+        
+        if(grade ==10){
+            System.out.println("");
+            System.out.println("---------------------------------------------");
+            System.out.println("Courses avaliable to registor in grade (10): ");
+            System.out.println("---------------------------------------------");
+            System.out.println(grade10);
+        }else if (grade==11){
+            System.out.println("");
+            System.out.println("---------------------------------------------");
+            System.out.println("Courses avaliable to registor in grade (11): ");
+            System.out.println("---------------------------------------------");
+            System.out.println(grade11);
+        }else if (grade==12){
+            System.out.println("");
+            System.out.println("---------------------------------------------");
+            System.out.println("Courses avaliable to registor in grade (12): ");
+            System.out.println("---------------------------------------------");
+            System.out.println(grade12);
+        }else{
+            System.out.println("");
+            System.out.println("No avalibale courses for this grade");
+
+       }
+
+       }
 }
