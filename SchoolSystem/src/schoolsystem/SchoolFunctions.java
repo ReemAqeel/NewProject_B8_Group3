2a07d51597334b8d6da4f2354f1ecbfaef990d5c
ReemAqeel <ReemAQ23@outlook.com>
2023-02-05 16:09:20 +0300

    delete course method done by Atheer + print student schedual done by Reem

--- SchoolSystem/src/schoolsystem/SchoolFunctions.java
+++ SchoolSystem/src/schoolsystem/SchoolFunctions.java
@@ -6,14 +6,46 @@ import java.util.ArrayList;
 
 public class SchoolFunctions {
     
+    //Method to print student schedual 
+    public static void print(ArrayList<String> STDschedual){
+        System.out.println("*******************");
+        System.out.println("Student Schedual ");
+        System.out.println("*******************");
+        System.out.println("-----------------------");
+        for (int i=0;i<STDschedual.size();i++){
+            String crr=STDschedual.get(i);
+            System.out.println(crr);
+            System.out.println("-----------------------");
+        }
+    }
+    
     //Add courses to students method 
     public static void ADDCOURSE(ArrayList<String> STDschedual,String courseName){
         if(STDschedual.contains(courseName)){
             System.out.println("The course is already in the student schedual");
+            SchoolFunctions.print(STDschedual);
         }else{
             STDschedual.add(courseName);
-            System.out.println("Course is added to student schedual ");
+            System.out.println("Course is successfully added to student schedual ");
+            SchoolFunctions.print(STDschedual);
         }
+    }
+    ////////////////////////////////////////////////
+    
+    //Delete courses to student method 
+     public static void DELETECOURSE(ArrayList<String> STDschedual, String courseName) {
+        if(STDschedual.contains(courseName)){
+            STDschedual.remove(courseName);
+            System.out.println("");
+            System.out.println("Course is successfully removed from your schedual  ");
+            SchoolFunctions.print(STDschedual);
+            
+        }else{
+            System.out.println("");
+            System.out.println("course is not on your schedual, please try again");
+            SchoolFunctions.print(STDschedual);
+        }
+        
     }
     
 }
